70ee689313c5e2001bb974bcae78fd19ab87a55d
Carolina <carolinamilanorg@gmail.com>
2022-12-27 22:28:40 -0300

    more modifications

--- assets/app/main.js
+++ assets/app/main.js
@@ -1,9 +1,23 @@
-async function converterAPI () {
-    const API = 'https://api.adviceslip.com/advice';
-    const conselho = await fetch(API);
-    const conselhoConvertido = await conselho.json();
+const btn = document.querySelector('[data-btn]');
 
-    console.log(conselhoConvertido)
-}
+    async function buscarConselho () {
+        const API = 'https://api.adviceslip.com/advice';
+        const conselho = await fetch(API);
+        const conselhoConvertido = await conselho.json();
 
-converterAPI()
+            escreverConselho(conselhoConvertido);
+
+            return conselhoConvertido;
+    }
+
+    function escreverConselho(objConselho) {
+        const num = document.querySelector('[data-num]');
+        const conselho = document.querySelector('[data-advice]');
+
+            num.innerHTML = objConselho.slip.id;
+            conselho.innerHTML = objConselho.slip.advice;
+    }
+
+            btn.addEventListener('click', () => buscarConselho())
+
+            
